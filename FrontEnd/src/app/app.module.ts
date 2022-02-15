c3de53b51f6bd6d9bb2fcdc05fe73bb5f97063b1
Hassan Askar <hassanemad15697@gmail.com>
2022-02-15 12:43:59 +0100

    adding signup form component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
