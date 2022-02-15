c15e58a31c4495d5d3985b074db1fcdec8e4cda0
Hassan Askar <hassanemad15697@gmail.com>
2022-02-15 15:26:31 +0100

    handling routes by routerLink=""

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,8 +11,12 @@ import {RouterModule, Routes} from "@angular/router";
 
 const routes : Routes =[
   {path: 'signup' , component : SignupComponent},
+  {path: 'signin' , component : SigninComponent},
   {path: 'employee' , component : EmployeeContentComponent},
-  {path: 'actions' , component : OptionsComponent}
+  {path: 'actions' , component : OptionsComponent},
+  {path: '' , component : EmployeeContentComponent},
+  {path: '**' , component : EmployeeContentComponent}
+
 ];
 
 
