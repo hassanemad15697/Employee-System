7298929b60b0bb5447d600ed9c159516c1b43660
Hassan Askar <hassanemad15697@gmail.com>
2022-02-15 14:48:28 +0100

    creating employee class to model the employee data from database in frontend

---
+++ FrontEnd/src/app/model/employee.ts
@@ -0,0 +1,23 @@
+export class Employee {
+
+  id : number;
+  name : string;
+  gender : string;
+  age : string;
+  phone : string;
+  address : string;
+  createdDate : Date;
+  updatedDate : Date;
+
+
+  constructor(id: number, name: string, gender: string, age: string, phone: string, address: string, createdDate: Date, updatedDate: Date) {
+    this.id = id;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.phone = phone;
+    this.address = address;
+    this.createdDate = createdDate;
+    this.updatedDate = updatedDate;
+  }
+}
